d3ec36d8e270887b2f0766d7278bfe5324f088a2
RamaKrishna Manchana <95981597+ramamanchana@users.noreply.github.com>
2024-07-19 21:41:45 -0500

    Update 001-strategicplanning-needsassesment.py

--- 001-strategicplanning-needsassesment.py
+++ 001-strategicplanning-needsassesment.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # Sample Data
@@ -30,3 +31,12 @@ new_data = pd.DataFrame({
 # Prediction
 predicted_feasibility = model.predict(new_data)
 print(f"Predicted Project Feasibility: {predicted_feasibility[0]}")
+
+# Plot
+plt.scatter(df['business_objectives'], df['project_feasibility'], color='blue', label='Actual')
+plt.scatter(new_data['business_objectives'], predicted_feasibility, color='red', label='Predicted')
+plt.xlabel('Business Objectives')
+plt.ylabel('Project Feasibility')
+plt.title('Project Feasibility Prediction')
+plt.legend()
+plt.show()
